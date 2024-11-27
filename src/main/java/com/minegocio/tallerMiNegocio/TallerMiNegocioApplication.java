39f0878ad947671b734e864620707875a6ae17cb
Jose <jose.luis22_@hotmail.es>
2024-11-27 17:10:01 -0500

    create repo

---
+++ src/main/java/com/minegocio/tallerMiNegocio/TallerMiNegocioApplication.java
@@ -0,0 +1,13 @@
+package com.minegocio.tallerMiNegocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerMiNegocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerMiNegocioApplication.class, args);
+	}
+
+}
